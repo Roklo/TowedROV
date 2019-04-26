5a5f5b6f98c477d9430746ae4a49686e6cba4768
RobinThorholm <rocio944@gmail.com>
2019-04-26 15:27:39 +0200

    *Updated autoupdater version controll

--- AutoUpdater/src/autoupdater/AutoUpdater.java
+++ AutoUpdater/src/autoupdater/AutoUpdater.java
@@ -1,7 +1,9 @@
 package autoupdater;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -16,18 +18,35 @@ public class AutoUpdater
         String sUrl = "https://github.com/Roklo/TowedROV/archive/master.zip";
         String destinationZip = "C://Temp//file.zip";
         String destination = "C://Temp";
+        String destinationVersion = "C:\\temp\\TowedROV-master\\TowedROV_GUI\\PublishedVersion\\Version.txt";
 
+        
         URL url = new URL(sUrl);
         ZipExtractor zipE = new ZipExtractor();
 
         //File where to be downloaded
-        File file = new File(destinationZip);
+        File filezip = new File(destinationZip);
 
         System.out.println("Starting to download file...");
-        URLReader.copyURLToFile(url, file);
-        System.out.println("Starting to unzip");
+        URLReader.copyURLToFile(url, filezip);
+        System.out.println("Starting to unzip...");
         zipE.unzip(destinationZip, destination);
         System.out.println("Unzip complete");
+        System.out.println("Checking version...");
+
+        File versionFile = new File(destinationVersion);
+        String newVersion = "0.0.0";
+        String oldVersion = "0.0.0";
+
+        BufferedReader br = new BufferedReader(new FileReader(versionFile));
+
+        String st;
+        while ((st = br.readLine()) != null)
+        {
+            newVersion = st;
+        }
+        System.out.println("New version is: " + newVersion);
+        System.out.println("Old version is: " + oldVersion);
     }
 
 }
